1463bf55aae503d28fefa9362e0d8093422c9f53
Rowan <36411655+RowanvdZ@users.noreply.github.com>
2020-02-05 17:42:45 +0100

    Navigation animation

--- js/custom.js
+++ js/custom.js
@@ -2,25 +2,25 @@
     Version: 1.0
 /****************************************** */
 
-(function($) {
+(function ($) {
     "use strict";
 
     /* ==============================================
     Fixed menu
     =============================================== */
-    
-	$(window).on('scroll', function () {
-		if ($(window).scrollTop() > 50) {
-			$('.top-navbar').addClass('fixed-menu');
-		} else {
-			$('.top-navbar').removeClass('fixed-menu');
-		}
-	});
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 0) {
+            $('.top-navbar').addClass('fixed-menu');
+        } else {
+            $('.top-navbar').removeClass('fixed-menu');
+        }
+    });
 
     /* ==============================================
     Back top
     =============================================== */
-    jQuery(window).scroll(function() {
+    jQuery(window).scroll(function () {
         if (jQuery(this).scrollTop() > 1) {
             jQuery('.dmtop').css({
                 bottom: "10px"
@@ -35,57 +35,105 @@
     /* ==============================================
 	Loader -->
 	=============================================== */
-	
-	$(window).load(function() {
+
+    $(window).load(function () {
         $("#preloader").on(500).fadeOut();
         $(".preloader").on(600).fadeOut("slow");
-		$('.loader-container').addClass('done');
-		$('.progress-br').addClass('done');	 
+        $('.loader-container').addClass('done');
+        $('.progress-br').addClass('done');
     });
-	
+
 	/* ==============================================
 		Scroll to top  
 	============================================== */
-		
-	if ($('#scroll-to-top').length) {
-		var scrollTrigger = 100, // px
-			backToTop = function () {
-				var scrollTop = $(window).scrollTop();
-				if (scrollTop > scrollTrigger) {
-					$('#scroll-to-top').addClass('show');
-				} else {
-					$('#scroll-to-top').removeClass('show');
-				}
-			};
-		backToTop();
-		$(window).on('scroll', function () {
-			backToTop();
-		});
-		$('#scroll-to-top').on('click', function (e) {
-			e.preventDefault();
-			$('html,body').animate({
-				scrollTop: 0
-			}, 700);
-		});
-	}
-	
+
+    if ($('#scroll-to-top').length) {
+        var scrollTrigger = 100, // px
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#scroll-to-top').addClass('show');
+                } else {
+                    $('#scroll-to-top').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        $("#home").on("click", function (e) {
+            e.preventDefault();
+            if (!$(window).scrollTop() == 0) {
+                $('html,body').animate({
+                    scrollTop: 5
+                }, 700);
+            }
+        });
+        $('#scroll-to-top').on('click', function (e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
+
+    /* ==============================================
+		Scroll to section from navigation
+    ============================================== */
+    var extraOffset;
+    $(".nav-item").click(function (e) {
+        e.preventDefault();
+        $('.nav-item').removeClass("active");   
+         $(this).addClass("active");
+        
+        if (100 > $(window).scrollTop()) {
+            extraOffset = 100;
+
+        } else {
+            extraOffset = 20;
+        }
+
+
+        var section = "#" + $(this).attr('id') + "-section";
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(section).offset().top - extraOffset
+        }, 700);
+    });
+
+    $(".hover-btn-new").click(function (e) {
+        e.preventDefault();        
+        if (100 > $(window).scrollTop()) {
+            extraOffset = 100;
+
+        } else {
+            extraOffset = 20;
+        }
+        var section = "#" + $(this).attr('id') + "-section";
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(section).offset().top - extraOffset
+        }, 700);
+    });
+    
+
+  
+
     /* ==============================================
      Fun Facts -->
      =============================================== */
 
     function count($this) {
         var current = parseInt($this.html(), 10);
-        current = current + 50; /* Where 50 is increment */
+        current = current + 10000; /* Where 50 is increment */
         $this.html(++current);
         if (current > $this.data('count')) {
             $this.html($this.data('count'));
         } else {
-            setTimeout(function() {
+            setTimeout(function () {
                 count($this)
             }, 30);
         }
     }
-    $(".stat_count, .stat_count_download").each(function() {
+    $(".stat_count, .stat_count_download").each(function () {
         $(this).data('count', parseInt($(this).html(), 10));
         $(this).html('0');
         count($(this));
@@ -94,7 +142,7 @@
 	/* ==============================================
      Bootstrap Touch Slider -->
      =============================================== */
-		$('#carouselExampleControls').bsTouchSlider();
+    $('#carouselExampleControls').bsTouchSlider();
     /* ==============================================
      Tooltip -->
      =============================================== */
@@ -104,28 +152,28 @@
     /* ==============================================
      Contact -->
      =============================================== */
-    jQuery(document).ready(function() {
-        $('#contactform').submit(function() {
+    jQuery(document).ready(function () {
+        $('#contactform').submit(function () {
             var action = $(this).attr('action');
-            $("#message").slideUp(750, function() {
+            $("#message").slideUp(750, function () {
                 $('#message').hide();
                 $('#submit')
                     .after('<img src="images/ajax-loader.gif" class="loader" />')
                     .attr('disabled', 'disabled');
                 $.post(action, {
-                        first_name: $('#first_name').val(),
-                        last_name: $('#last_name').val(),
-                        email: $('#email').val(),
-                        phone: $('#phone').val(),
-                        select_service: $('#select_service').val(),
-                        select_price: $('#select_price').val(),
-                        comments: $('#comments').val(),
-                        verify: $('#verify').val()
-                    },
-                    function(data) {
+                    first_name: $('#first_name').val(),
+                    last_name: $('#last_name').val(),
+                    email: $('#email').val(),
+                    phone: $('#phone').val(),
+                    select_service: $('#select_service').val(),
+                    select_price: $('#select_price').val(),
+                    comments: $('#comments').val(),
+                    verify: $('#verify').val()
+                },
+                    function (data) {
                         document.getElementById('message').innerHTML = data;
                         $('#message').slideDown('slow');
-                        $('#contactform img.loader').fadeOut('slow', function() {
+                        $('#contactform img.loader').fadeOut('slow', function () {
                             $(this).remove()
                         });
                         $('#submit').removeAttr('disabled');
@@ -136,12 +184,12 @@
             return false;
         });
     });
-	
+
     /* ==============================================
      Code Wrapper -->
      =============================================== */
 
-    $('.code-wrapper').on("mousemove", function(e) {
+    $('.code-wrapper').on("mousemove", function (e) {
         var offsets = $(this).offset();
         var fullWidth = $(this).width();
         var mouseX = e.pageX - offsets.left;
@@ -165,7 +213,7 @@
             transition: 'none'
         });
     });
-    $('.divider-wrapper').on("mouseleave", function() {
+    $('.divider-wrapper').on("mouseleave", function () {
         $(this).parent().find('.divider-bar').css({
             left: '50%',
             transition: 'all .3s'
